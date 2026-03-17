7f904e264de82a74decc941fcbdfb9ed7298200a
Liam Gallagher <liamkeithgallagher@gmail.com>
2026-03-17 15:51:54 -0400

    Added boiler plate files

---
+++ index.js
@@ -0,0 +1,8 @@
+// Wait until the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("btn");
+
+    button.addEventListener("click", () => {
+        alert("Button clicked!");
+    });
+});
