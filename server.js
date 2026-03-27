309a502436aa0ef52af60adc30d11237a9c69a3d
JBitterUT <113638925+JBitterUT@users.noreply.github.com>
2026-03-27 16:26:03 -0400

    small stupid fixes
    Fixed a handfull of small stupid errors that were preventing node.js from functioning (im dumb). Added README.MD to explain server functionality in depth.

--- server.js
+++ server.js
@@ -6,16 +6,37 @@
  * This is the server.js file that exists server-side to get functionallity to the clients.
 */
 
-const express = require('express');
+/**
+ * App uses Express, which is just a Node.js web-application framework
+ * 
+ * @const express   imports express module
+ */
+const express = require('express')
 
-const app = express();
-const PORT = 8080;
 
-app.use(express.static('public'));
-app.use(express.static('views)'));
+/**
+ * @const app       express application instance
+ * @const PORT      The port that the server is running on
+ */
+const app = express()
+const PORT = 8080
 
-app.get("/", (res, req) => {
+/**
+ * These lines allow express to view static files. In short, this lets express use HTML, CSS, Javascrips, and Image files
+ * in the ./views and ./public directories
+ */
+app.use(express.static('views'))
+app.use(express.static('public'))
+
+
+/**
+ * gets the default root. On connection, client is sent to "[WEBHOST]/". This catches when someone asks for that default 
+ * "/" root, and sends them index.html in response.
+ */
+app.get("/", (req, res) => {
     res.send('index.html')
 })
-
+/**
+ * Starts the server listening on PORT, and logs to the console that server has successfully started on PORT
+ */
 app.listen(PORT, () => console.log("Server started on port: " + PORT))
